65195cb4a97a51daeab6c84ee7f7784bf3b81c3a
albi <albijon.re@gmail.com>
2023-12-14 18:15:20 -0500

    done

--- routes/alcohols.js
+++ routes/alcohols.js
@@ -4,6 +4,18 @@ var alcoholCtrl = require('../controllers/alcohols')
 
 router.get('/', alcoholCtrl.index)
 
+router.get('/new', alcoholCtrl.new)
+
 router.get('/:id', alcoholCtrl.show)
+
+router.get('/:id/edit', alcoholCtrl.edit)
+
+router.post('/', alcoholCtrl.create)
+
+router.delete('/:id', alcoholCtrl.delete)
+
+router.put('/:id', alcoholCtrl.update)
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var alcoholCtrl = require('../controllers/alcohols')
-const alcohol = require('../models/alcohol')
+const Alcohol = require('../models/alcohol')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'alcohols',
-    alcohols: alcohol.getAll()
+    alcohols: Alcohol.getAll()
   })
 });
 
